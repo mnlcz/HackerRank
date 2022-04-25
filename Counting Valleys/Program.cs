2f6d78a1de5cd4a15b400df652a79714cbe40e59
mnlcz <manuel.lopez.cosmitz@gmail.com>
2022-04-25 07:12:27 -0300

    Added "Counting Valleys" problem

---
+++ Counting Valleys/Program.cs
@@ -0,0 +1,22 @@
+﻿int CoutingValleys(int steps, string path)
+{
+    int valleys = 0;
+    int position = 0; // sea level
+    foreach(char c in path)
+    {
+        if(c == 'D')
+            position--;
+        else
+        {
+            position++;
+            if(position == 0)
+                valleys++;
+        }
+    }
+    return valleys;
+}
+
+string path1 = "UDDDUDUU";
+string path2 = "DDUUDDUDUUUD";
+Console.WriteLine(CoutingValleys(8, path1));
+Console.WriteLine(CoutingValleys(12, path2));
