37f71aabf064a5125d40888ffe0dab9dcf10c134
mnlcz <manuel.lopez.cosmitz@gmail.com>
2022-04-28 12:17:25 -0300

    Added Day 17

---
+++ 30 Days of Code/Days/Day17.cs
@@ -0,0 +1,40 @@
+﻿namespace _30_Days_of_Code.Days;
+
+public static class Day17
+{
+    // Remove public modifier when pasting in HackerRank
+    public class Calculator
+    {
+        // HackerRank expects power in lowercase.
+        public int Power(int n, int p)
+        {
+            if (n < 0 || p < 0)
+                throw new ArgumentException("n and p should be non-negative");
+            return (int)Math.Pow(n, p);
+        }
+    }
+
+    public static void Test()
+    {
+        Calculator c = new();
+        Console.WriteLine(c.Power(3, 5));
+        Console.WriteLine(c.Power(2, 4));
+        try
+        {
+            Console.WriteLine(c.Power(-1, -2));
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.Message);
+        }
+
+        try
+        {
+            Console.WriteLine(c.Power(-1, 3));
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.Message);
+        }
+    }
+}
--- 30 Days of Code/Program.cs
+++ 30 Days of Code/Program.cs
@@ -1,3 +1,3 @@
 ﻿using _30_Days_of_Code.Days;
 
-Day16.Solution();
+Day17.Test();
