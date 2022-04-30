637e6f16e8c2c74e4f9adb0162013fa5ee342c5c
mnlcz <manuel.lopez.cosmitz@gmail.com>
2022-04-30 16:25:48 -0300

    Added Day 19

---
+++ 30 Days of Code/Days/Day19.cs
@@ -0,0 +1,33 @@
+﻿namespace _30_Days_of_Code.Days;
+
+public static class Day19
+{
+    public interface AdvancedArithmetic
+    {
+        int divisorSum(int n);
+    }
+
+    public class Calculator : AdvancedArithmetic
+    {
+        public int divisorSum(int n)
+        {
+            var sum = 0;
+            for (var i = 1; i <= n; i++)
+            {
+                if (n % i == 0)
+                {
+                    sum += i;
+                }
+            }
+
+            return sum;
+        }
+    }
+
+    public static void Test()
+    {
+        Calculator c = new();
+        Console.WriteLine(c.divisorSum(25));
+        Console.WriteLine(c.divisorSum(20));
+    }
+}
--- 30 Days of Code/Program.cs
+++ 30 Days of Code/Program.cs
@@ -1,3 +1,3 @@
 ﻿using _30_Days_of_Code.Days;
 
-Day17.Test();
+Day19.Test();
