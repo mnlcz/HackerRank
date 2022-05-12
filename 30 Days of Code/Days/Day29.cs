b3b9114d44587eea37e84b40f9c2530940201901
mnlcz <manuel.lopez.cosmitz@gmail.com>
2022-05-12 06:56:04 -0300

    Added last day

---
+++ 30 Days of Code/Days/Day29.cs
@@ -0,0 +1,15 @@
+﻿namespace _30_Days_of_Code.Days;
+
+public static class Day29
+{
+    public static int BitwiseAnd(int n, int k)
+    {
+        var set = Enumerable.Range(1, n).ToList();
+        var max = int.MinValue;
+        for (var i = 0; i < n - 1; i++)
+            for (var j = i + 1; j < n; j++)
+                if((set[i] & set[j]) < k && (set[i] & set[j]) > max)
+                    max = (set[i] & set[j]);
+        return max;
+    }
+}
